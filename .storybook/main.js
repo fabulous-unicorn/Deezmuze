57410d92e991d29e6e889dec29330b79d797f7c3
alesya <alesya.volosach@gmail.com>
2020-03-24 19:23:46 +0300

    add storybook and local css-vars

---
+++ .storybook/main.js
@@ -0,0 +1,7 @@
+module.exports = {
+	webpackFinal: (config) => console.dir(config, { depth: null }) || config,
+};
+	
+module.exports = {
+	stories: ['../src/**/*.stories.[tj]s'],
+};
--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
+import './assets/fonts/Roboto/Roboto-Bold.ttf'
+import './assets/fonts/Roboto/Roboto-Medium.ttf'
+import './assets/fonts/Roboto/Roboto-Regular.ttf'
+import './assets/fonts/Roboto/Roboto-Light.ttf'
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
