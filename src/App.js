f40ed924d6d71851de92d55b209979e65ca30539
alesya <alesya.volosach@gmail.com>
2020-03-27 18:32:30 +0300

    changed buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
 import React from 'react';
 import './App.css';
 import { Button } from './ui/core/Button/Button';
+import {IconMix, IconPlay} from './ui/core/Icon/icons';
 
 function App() {
+  const size = 'xs';
   return (
     <div className="App">
       <header className="App-header">
-        <Button view='primary'>QWERTYUIOP</Button>
+        <Button
+          view='primary'
+          size='xl'
+          onClick={()=> { console.log("it's button click")}}
+        >
+          ЭТО КНОПКА
+        </Button>
+
+        <Button
+          view='secondary'
+          size='xl'
+          component='a'
+          onClick={()=> { console.log("it's button-a click")}}
+        >
+            И ЭТО ТОЖЕ КНОПКА
+        </Button>
+
+        <Button
+          view='clear'
+          icon={<IconPlay size='xl'/>}
+          size='xl'
+          component='button'
+        />
+
+        <Button
+          view='secondary'
+          size='xl'
+          component='button'
+        >
+          <IconMix size='xl'/>
+        </Button>
+
+        <Button
+          view='primary'
+          rounded={true}
+          icon={<IconPlay size='s'/>}
+          component='button'
+        />
+
       </header>
     </div>
   );
--- src/ui/core/Button/Button.js
+++ src/ui/core/Button/Button.js
@@ -1,13 +1,94 @@
 import React from 'react'
 import styles from './Button.module.css'
 import cn from 'classnames'
+import pt from 'prop-types'
 
 export function Button(props) {
-  
-	const classNames = {
-		[styles.button]: true,
-		[styles[props.view]]: props.view
-	}
 
-	return (<button className={cn(classNames)}>{props.children}</button>)
+	const {
+		className,
+		size,
+    view,
+    rounded,
+		children,
+		noStyle,
+		icon,
+		onClick,
+		hint,
+		testId,
+		component,
+		...rest
+	} = props;
+
+	const Component = component;
+
+	const classes = noStyle
+		? cn(styles.noStyle, { [className]: Boolean(className) })
+		: cn({
+				[styles.button]: true,
+        [styles.withIcon]: Boolean(icon),
+        [styles.onlyIcon]: icon && !children,
+				[styles[`size_${size}`]]: size,
+        [styles[`view_${view}`]]: view,
+        [styles.rounded]: rounded,
+				[className]: Boolean(className),
+			});
+
+  const commonProps = {
+    'className': classes,
+    'onClick': onClick,
+    'title': hint,
+    'data-test-id': testId || props['data-test-id'],
+  };
+
+  const iconView = props.icon && <span className={styles.icon}>{icon}</span>;
+
+  const childClasses = cn({
+    [styles.content]: true
+  });
+
+  const buttonChild = iconView ? (
+    <span>
+      {iconView}
+      {children && <div className={styles.text}>{children}</div>}
+    </span>
+  ) : (
+    <>{children}</>
+  );
+
+  return <Component {...commonProps}>{buttonChild}</Component>
+  // return (<button className={classes} onClick={()=>{console.log('click')}}>{props.children}</button>)
 }
+
+
+Button.displayName = 'Button';
+
+Button.propTypes = {
+  noStyle: pt.bool,
+  view: pt.oneOf(['primary', 'secondary', 'clear']),
+  rounded: pt.bool,
+  size: pt.oneOf(['l', 'xl']),
+  icon: pt.node,
+  hint: pt.string,
+  component: pt.elementType,
+  testId: pt.string,
+  children: pt.node,
+  disabled: pt.bool,
+  centerContent: pt.bool,
+};
+
+Button.defaultProps = {
+  noStyle: false,
+  view: 'action',
+  size: 'xl',
+  icon: undefined,
+  rounded: false,
+  // iconPosition: 'right',
+  component: 'button',
+  hint: undefined,
+  testId: undefined,
+  children: undefined,
+  disabled: undefined,
+  // isAdaptive: false,
+  centerContent: true,
+};
